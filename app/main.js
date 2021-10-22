adb0bbb307ac383c07445a1e4d7eff45d750acdf
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2021-10-22 21:42:06 +0300

    Fill patient list with data from array

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 appInit = ()=>{
-    $(".action-link").click(onActionLinkClick);  
+    $(".action-link").click(onActionLinkClick); 
+    renderTable();
 }
 hidePages = () => {
     $(".component").hide();
@@ -11,5 +12,21 @@ onActionLinkClick = (e) => {
     var target = $(e.target).data("target");
     hidePages();
     navigate(target);
+}
+renderTable = ()=> {
+    var x = $(".patient-list-body"); 
+   
+    for( let i=0; i<patientsData.length; i++){
+        x.append('<tr>'+
+        '<td>'+patientsData[i].ID+'</td>'+
+        '<td>'+patientsData[i].fname+' '+patientsData[i].mname+' '+patientsData[i].lname+'</td>'+
+        '<td>'+patientsData[i].email+'</td>'+
+        '<td>'+patientsData[i].gender+'</td>'+
+        '<td>'+patientsData[i].DOB+'</td>'+
+        '<td>'+patientsData[i].Active+'</td>'+
+        '<td>'+'Created By:'+patientsData[i].CreatedBy+', '+patientsData[i].creationDate+'</td>'+
+        '<td><button class="btn btn-info action-link" data-target="#user-edit">Edit</button></td>'+
+        +'</tr>');
+    }
 }
 $(document).ready(appInit);
