f5d9299efa205cce49c6372b86263e4cc0a202b5
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2021-10-27 21:45:28 +0300

    Templating Use template Html tag

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 appInit = ()=>{
-    $(".action-link").click(onActionLinkClick); 
     renderTable();
+    $(".action-link").click(onActionLinkClick); 
 }
 hidePages = () => {
     $(".component").hide();
@@ -14,36 +14,22 @@ onActionLinkClick = (e) => {
     navigate(target);
 }
 renderTable = ()=> {
-    var board = $(".patient-list-body"); 
-    var str = '<tr>'+
-    '<td>{{ID}}</td>'+
-    '<td>{{fname}} {{mname}} {{lname}}</td>'+
-    '<td>{{email}}</td>'+
-    '<td>{{gender}}</td>'+
-    '<td>{{DOB}}</td>'+
-    '<td>{{Active}}</td>'+
-    '<td>Created By: {{CreatedBy}} {{creationDate}}</td>'+
-    '<td><button class="btn btn-info action-link" data-target="#user-edit">Edit</button></td></tr>';
+    var list = $(".patient-list-body"); 
+    var str = $(".template").html(); 
 
     for( let i=0; i<patientsData.length; i++){
         var templete = renderTemplete(str,patientsData[i]);
-        board.append(templete);
+        list.append(templete);
     }
 }
 
 renderTemplete = (templateText, data) =>{   
-    var myString = templateText;
-    var key;
-    var indexOpen;
-    var indexClose;
-
+    var myString = templateText; 
     for(let i=0; i<myString.length; i++){
-        if(myString.includes("{{")){ 
-            indexOpen = myString.indexOf("{")+2;
-            indexClose =  myString.indexOf("}");
-        
-            key = myString.substring(indexOpen,indexClose);
-
+        if(myString.includes("{{")){     
+            var indexOpen = myString.indexOf("{")+2;
+            var indexClose =  myString.indexOf("}");
+            var key = myString.substring(indexOpen,indexClose);
             //=== replace
             myString = myString.replace("{{"+key+"}}", data[key]);
         } 
