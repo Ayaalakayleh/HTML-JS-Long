c0cc6bc5960c7c86756b9f68682b490c5c7decf4
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2021-11-15 21:33:15 +0200

    validation_create function to separate validation business

--- app/main.js
+++ app/main.js
@@ -18,7 +18,6 @@ renderTable = ()=> {
 }
 var patientID;
 var formMode ;
-var haveError;
 onEditButtonClick = (e)=>{
     var IdRowTarget =  $(e.target).data("id");
     $('.patient-id').html(IdRowTarget);
@@ -40,16 +39,16 @@ open=(id)=>{
     }
 }
 onSaveButtonClick =(e)=>{ 
-    
-    let resultCheck = checkFields();
-    if(!resultCheck){
+   let checkValidation =  ValidateForm();
+
+   if(checkValidation){
         var currentPatientData = GetControlsData (patientID);
         if(formMode == "Edit"){
             DataService.edit(currentPatientData);
         }     
         else{
             DataService.add(currentPatientData);     
-        }
+        } 
         routerEngine.onActionLinkClick(e);
     }
     else{
@@ -61,47 +60,42 @@ errorAlert=(element)=>{
     element.parent('.form-group').addClass("has-error");
     element.siblings("span").html("invalid");
 }
-successAlert=(element)=>{
-    element.parent('.form-group').removeClass("has-error");
-    element.parent('.form-group').addClass("has-success");
-    element.siblings("span").hide();
+successAlert=()=>{
+    $("input").parent('.form-group').removeClass("has-error");
+    $("input").siblings("span").hide();
 }
-checkValidData=(element,pattern)=>{
-    
-    if(!pattern.test(element.val()) ){
-        errorAlert(element);
-        return true;   
-    }
-    if(element.val() === ''){
-        errorAlert(element);
-        return true;  
-    }
-    else{
-        successAlert(element);
-        return false;
-    } 
-}
-checkFields=()=>{
+ValidateForm=()=>{
+    var isValid =true;
     let email = $("#EmailInput");
     let fname = $("#FirstNameInput");
-    let nname = $("#MiddleNameInput");
+    let mname = $("#MiddleNameInput");
     let lname = $("#LastNameInput");
     let emailPattern =  /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     let namePattern = /^[a-zA-Zء-ي]/;     
     
-    var result =[];
-    result.push(checkValidData(email,emailPattern)); 
-    result.push(checkValidData(fname,namePattern)) ;
-    result.push(checkValidData(nname,namePattern));
-    result.push(checkValidData(lname,namePattern));    
+    if( !emailPattern.test(email.val()) || email.val() === ''){
+        isValid = false;
+        errorAlert(email);
+    }  
+    if( !namePattern.test(fname.val()) || fname.val() === ''){
+        isValid = false;
+        errorAlert(fname);
+    }
+    if( !namePattern.test(mname.val()) || mname.val() === ''){
+        isValid = false;
+        errorAlert(mname);
+    }
+    if( !namePattern.test(lname.val()) || lname.val() === ''){
+        isValid = false;
+        errorAlert(lname);
+    }
     
-    for(let i=0;i<result.length;i++){
-        if(result[i] == true){
-            return true;
-        }
-        else{
-            return false;
-        }
+    if(isValid){
+        successAlert();
+        return true;
+    }
+    else{
+        return false;
     }
 }
 LoadControlsData = (patientData) =>{
