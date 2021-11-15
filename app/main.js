285a404fdbe9329c660f6f617fcc1b51adb8e808
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2021-11-15 21:25:00 +0200

    validation_add validation functionality to form fields

--- app/main.js
+++ app/main.js
@@ -6,7 +6,6 @@ appInit = ()=>{
     $(".add-btn").click(onAddButtonClick);
     $(".confirm-btn").click(onConfirmDeleteBtnClick);
 }
-
 renderTable = ()=> {
     $(".patient-list-body").empty();
     var list = $(".patient-list-body"); 
@@ -19,6 +18,7 @@ renderTable = ()=> {
 }
 var patientID;
 var formMode ;
+var haveError;
 onEditButtonClick = (e)=>{
     var IdRowTarget =  $(e.target).data("id");
     $('.patient-id').html(IdRowTarget);
@@ -39,15 +39,70 @@ open=(id)=>{
         LoadControlsData(patientData); 
     }
 }
-onSaveButtonClick =()=>{
-    var currentPatientData = GetControlsData (patientID);
-    if(formMode == "Edit"){
-        DataService.edit(currentPatientData);
-    } 
+onSaveButtonClick =(e)=>{ 
+    
+    let resultCheck = checkFields();
+    if(!resultCheck){
+        var currentPatientData = GetControlsData (patientID);
+        if(formMode == "Edit"){
+            DataService.edit(currentPatientData);
+        }     
+        else{
+            DataService.add(currentPatientData);     
+        }
+        routerEngine.onActionLinkClick(e);
+    }
     else{
-        DataService.add(currentPatientData); 
+        return;
+    }
+    renderTable(); 
+}
+errorAlert=(element)=>{
+    element.parent('.form-group').addClass("has-error");
+    element.siblings("span").html("invalid");
+}
+successAlert=(element)=>{
+    element.parent('.form-group').removeClass("has-error");
+    element.parent('.form-group').addClass("has-success");
+    element.siblings("span").hide();
+}
+checkValidData=(element,pattern)=>{
+    
+    if(!pattern.test(element.val()) ){
+        errorAlert(element);
+        return true;   
+    }
+    if(element.val() === ''){
+        errorAlert(element);
+        return true;  
+    }
+    else{
+        successAlert(element);
+        return false;
     } 
-    renderTable();     
+}
+checkFields=()=>{
+    let email = $("#EmailInput");
+    let fname = $("#FirstNameInput");
+    let nname = $("#MiddleNameInput");
+    let lname = $("#LastNameInput");
+    let emailPattern =  /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    let namePattern = /^[a-zA-Zء-ي]/;     
+    
+    var result =[];
+    result.push(checkValidData(email,emailPattern)); 
+    result.push(checkValidData(fname,namePattern)) ;
+    result.push(checkValidData(nname,namePattern));
+    result.push(checkValidData(lname,namePattern));    
+    
+    for(let i=0;i<result.length;i++){
+        if(result[i] == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
 LoadControlsData = (patientData) =>{
     $("#FirstNameInput").val(patientData.fname);
@@ -75,7 +130,7 @@ LoadControlsData = (patientData) =>{
     var checkDate = moment(patientData.lastCheck).format('yyyy-MM-DD')
     $(".lastCheckInput").append($('<option>').html(checkDate));
 }
-GetControlsData = () =>{
+GetControlsData = () =>{   
     var fname   = $("#FirstNameInput").val();
     var mname   = $("#MiddleNameInput").val();
     var lname   = $("#LastNameInput").val();
